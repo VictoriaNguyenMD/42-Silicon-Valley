1f31a4e31d5e3257916a16afa502aa6d4415cd3a
Victoria Nguyen <vinguyen@e1z2r4p9.42.us.org>
2019-09-19 20:37:12 -0700

    ft_atoi.c

--- Projects/Libft/srcs/ft_atoi.c
+++ Projects/Libft/srcs/ft_atoi.c
@@ -6,25 +6,34 @@
 /*   By: vinguyen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 22:04:13 by vinguyen          #+#    #+#             */
-/*   Updated: 2019/09/16 22:54:25 by vinguyen         ###   ########.fr       */
+/*   Updated: 2019/09/19 20:37:04 by vinguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_atoi (const char *str)
 {
 	int sign;
+	int output;
    
 	sign = 1;
-	while (*str == ' ') 
+	output = 0;
+	while ((*str == ' ') || (*str == '\t') || (*str == '\v') ||
+	(*str == '\n') || (*str == '\f') || (*str == '\r')) 
 	{
 		str++;
 	}
 	if (*str == '-')
+	{
 		sign = -1;
-	if (*str >= 'a' && *str <= 'z')
-		return 
-		*str >= 'A' && *str <= 'Z')
-
-
-	return 0;
+		str++;
+	}
+	if (*str == '+')
+	{
+		str++;
+	}
+	while ((*str >= '0' && *str <= '9'))
+	{
+		output = (output * 10) + (*str++ - '0');
+	}	
+	return output * sign;
 }
