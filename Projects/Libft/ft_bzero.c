e86ae0c4835ef1459ea02e8f3131deaca3447300
Victoria Nguyen <vinguyen@e1z3r4p17.42.us.org>
2019-10-08 01:42:57 -0700

    Update ft_strlcat.c

---
+++ Projects/Libft/ft_bzero.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vinguyen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/01 14:30:28 by vinguyen          #+#    #+#             */
+/*   Updated: 2019/10/01 14:35:29 by vinguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	writes n zeroed bytes to the string s. If n = zero, then nothing occurs
+*/
+
+#include "libft.h"
+
+void	bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = (void*)0;
+		i++;
+	}
+}
---
+++ Projects/Libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcpy.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vinguyen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/01 14:47:23 by vinguyen          #+#    #+#             */
+/*   Updated: 2019/10/01 14:47:25 by vinguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Copies n bytes from the memory are src to memory area dst
+** Returns: dst
+*/
+
+#include "libft.h"
+
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n && *src != '\0')
+	{
+		dst[i] = src[i];
+	}
+	return dest;
+}
