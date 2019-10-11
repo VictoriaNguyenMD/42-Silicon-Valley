51a257cf13dfdc166960061e72102a48bd867351
Victoria Nguyen <vinguyen@e1z3r4p17.42.us.org>
2019-10-10 23:27:31 -0700

    Multiple Str Functions

--- Projects/Libft/libft.h
+++ Projects/Libft/libft.h
@@ -15,11 +15,14 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
 
 void *ft_memset(void *b, int c, size_t len);
-void bzero(void *s, size_t n);
+void ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 char *ft_strdup(const char *s1);
 size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dst, const char *src);
@@ -42,4 +45,16 @@ int	ft_isprint(int c);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
 
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int	ft_strequ(char const *s1, char const *s2);
+int	ft_strnequ(char const *s1, char const *s2, size_t n);
+void	ft_putchar(int c);
 #endif
